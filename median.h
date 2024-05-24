ab450f2c6d0b920efe0ae1ef0f1ddf287c564e1e
20222073yyh <yyh0377@naver.com>
2024-05-24 18:21:41 +0900

    .

---
+++ median.h
@@ -0,0 +1,12 @@
+#pragma once
+// median.h
+
+#ifndef GUARD_median_h
+#define GUARD_median_h
+
+
+#include "Vec.h" // Vec -> Vec
+
+double median(Vec<double>);
+
+#endif
---
+++ util.h
@@ -0,0 +1,43 @@
+// util.h
+
+#ifndef GUARD_util_h
+#define GUARD_util_h
+
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+
+
+#include "analysis.h"
+#include "Student_info.h"
+#include "Vec.h" // Vec -> Vec
+
+int printGreeting();
+void printMenu();
+
+void loadGrades(ifstream&, 
+	Vec<Student_info>&,
+	Vec<Student_info>&, 
+	Vec<Student_info>&,
+	string::size_type&
+);
+
+void fileSelect(int,
+	Vec<Student_info>&,
+	Vec<Student_info>&,
+	Vec<Student_info>&,
+	string::size_type&
+);
+
+int checkHw(Vec<Student_info>&, Vec<Student_info>&);
+
+// Overloaded from analysis.h - to handle a single Vec<Student_info> (not two)
+void write_analysis(ostream&, const string&,
+	double analysis(const Vec<Student_info>&),
+	const Vec<Student_info>&,
+	string);
+
+void analyzeGrades(Vec<Student_info>&, Vec<Student_info>&);
+void showScores(Vec<Student_info>&, string::size_type&);
+
+#endif
